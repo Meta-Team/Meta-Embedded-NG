7e83cdd4e0a7f22e1630dec6e8f728b03ed5acd9
Xiang Li <xiang.24@intl.zju.edu.cn>
2026-03-14 15:26:02 +0800

    refactor: organize head files

--- application/sentry/sentry.c
+++ application/sentry/sentry.c
@@ -1,30 +1,32 @@
-#include "sentry.h"
-#include "bsp_init.h"
-// #include "dt7_cmd.h"
-#include "vtm_cmd.h"
-#include "agv_chassis.h"
+// app
 #include "2yaw_gimbal.h"
+#include "agv_chassis.h"
+#include "sentry.h"
+#include "vtm_cmd.h"
+
+// module
 #include "ins_task.h"
 #include "sentry_task.h"
+
+// bsp
+#include "bsp_init.h"
 
 void SentryInit()
 {
     __disable_irq();
     BSPInit();
-    // RobotCMDInit();
     VTMCMDInit();
     AGVChassisInit();
-    // GimbalInit();
+    GimbalInit();
     SentryOSTaskInit();
     __enable_irq();
 }
 
 void SentryControlTask()
 {
-    // RobotCMDTask();
     VTMCMDTask();
     AGVChassisTask();
-    // GimbalTask();
+    GimbalTask();
 }
 
 void SentrySensorTask()
